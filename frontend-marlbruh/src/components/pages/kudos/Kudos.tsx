b81ca7e57332fbc6d377665607209470ba3e947d
LoganMartinez <lmartin9@trinity.edu>
2023-09-13 17:18:02 -0500

    added some stuff to kudos page

--- frontend-marlbruh/src/components/pages/kudos/Kudos.tsx
+++ frontend-marlbruh/src/components/pages/kudos/Kudos.tsx
@@ -1,7 +1,50 @@
+import { Box, Container, Loader, SimpleGrid } from "@mantine/core";
 import ComingSoon from "../reusablePages/ComingSoon";
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../../api/apiCalls";
+import { useAuth } from "../../../authentication/AuthContext";
+import { AxiosError } from "axios";
+import { errorNotification } from "../../../utilities/helperFunctions";
+import UserHandle from "../../reusableComponents/UserHandle";
 
 const Kudos = () => {
-  return <ComingSoon />;
+  const auth = useAuth();
+  const [allUsers, setAllUsers] = useState([] as User[]);
+
+  useEffect(() => {
+    getAllUsers(auth.authToken)
+      .then(({ data: users }) => {
+        setAllUsers(users);
+      })
+      .catch((err: AxiosError) => {
+        errorNotification(err.message);
+      });
+  }, []);
+
+  return (
+    <>
+      <SimpleGrid
+        cols={3}
+        breakpoints={[
+          { maxWidth: "62rem", cols: 3, spacing: "md" },
+          { maxWidth: "48rem", cols: 2, spacing: "sm" },
+          { maxWidth: "36rem", cols: 1, spacing: "sm" },
+        ]}
+      >
+        {allUsers ? (
+          allUsers.map((user) => (
+            <Box key={user.userId}>
+              <Container p="md">
+                <UserHandle user={user} />
+              </Container>
+            </Box>
+          ))
+        ) : (
+          <Loader />
+        )}
+      </SimpleGrid>
+    </>
+  );
 };
 
 export default Kudos;
