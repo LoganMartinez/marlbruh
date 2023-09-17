656409460981f51ea219a71af565deee576a3073
LoganMartinez <lmartin9@trinity.edu>
2023-09-17 12:53:19 -0500

    fixed visual bug with chapter select and translate tool

--- frontend-marlbruh/src/components/pages/bookclub/TranslateTool.tsx
+++ frontend-marlbruh/src/components/pages/bookclub/TranslateTool.tsx
@@ -50,7 +50,7 @@ const useStyles = createStyles((theme) => ({
     backgroundColor: theme.colors.dark[8],
     borderBottom: `2px solid ${theme.colors.dark[6]}`,
     borderTop: `2px solid ${theme.colors.dark[6]}`,
-    zIndex: 1000,
+    zIndex: 100,
   },
 
   wrSticky: {
